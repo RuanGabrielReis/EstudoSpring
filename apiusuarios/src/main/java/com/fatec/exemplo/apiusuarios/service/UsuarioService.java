f33f4ca71c7efc4d822183cd44ed2b5dc9ee97e7
RuanGabrielReis <ruangabreis28@gmail.com>
2026-08-12 12:22:54 -0300

    12/08

---
+++ apiusuarios/src/main/java/com/fatec/exemplo/apiusuarios/service/UsuarioService.java
@@ -0,0 +1,25 @@
+package com.fatec.exemplo.apiusuarios.service;
+
+
+import com.fatec.exemplo.apiusuarios.model.Usuario;
+import com.fatec.exemplo.apiusuarios.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> listarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deletar(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
