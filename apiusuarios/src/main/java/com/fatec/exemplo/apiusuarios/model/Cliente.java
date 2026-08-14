d69a93b124f3af9d74160c5a659dbb7d00de0875
RuanGabrielReis <ruangabreis28@gmail.com>
2026-08-14 11:08:28 -0300

    14/08

--- apiusuarios/src/main/java/com/fatec/exemplo/apiusuarios/model/Cliente.java
+++ apiusuarios/src/main/java/com/fatec/exemplo/apiusuarios/model/Cliente.java
@@ -19,4 +19,60 @@ public class Cliente {
     private String cpf;
     private String telefone;
     private String rg;
+
+    public Long getCod_cli() {
+        return cod_cli;
+    }
+
+    public void setCod_cli(Long cod_cli) {
+        this.cod_cli = cod_cli;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(LocalDate data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
 }
